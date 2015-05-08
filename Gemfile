4e8ced19e4f8dfab55b7572f2c7c4faa1bb66298
Owen Lynch <oclynch888@gmail.com>
2015-05-08 17:16:31 -0400

    termbox working

--- Gemfile
+++ Gemfile
@@ -1,2 +1,4 @@
 source 'https://rubygems.org'
-gem 'ncursesw', '~> 1.4.9'
+
+gem 'ncursesw', '~>1.4.9'
+gem 'rb_termbox', '~>0.2.0'
---
+++ termbox_test.rb
@@ -0,0 +1,12 @@
+require 'termbox'
+
+Termbox.initialize_library
+
+begin
+	Termbox.tb_init
+	Termbox.tb_change_cell 0, 0, ?@.ord, 6, 0
+	Termbox.tb_present
+	sleep 2
+ensure
+	Termbox.tb_shutdown
+end
