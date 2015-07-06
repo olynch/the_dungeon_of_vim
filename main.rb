af95aaa6fb87e946783f842f999a98462853ff64
Radvendii <dontsendmejunk@mailinator.com>
2015-07-06 09:02:33 -0400

    made some kind of a get-to-the-star game.

--- main.rb
+++ main.rb
@@ -2,9 +2,46 @@ require 'termbox'
 
 Termbox.initialize_library
 
-#class Map
-    #def initialize(map_file)
+#class Dungeon
+    #def self.parse(fn)
+        #File.open(fn, ?r) do |f|
+            #return f.each_line.map do |ln|
+                #ln.each_char.map do |c|
+                    #case c
+                    #when ?#
+                        #?#
+                    #else
+                        #nil
+                    #end
+                #end
+            #end
+        #end
+    #end
+    #def each(&block)
+        #@grid.each do |row|
+            #row.each do |thing|
+                #yield thing unless thing.nil?
+            #end
+        #end
+    #end
+    #def initialize(li=[])
+        #@grid = li
+        #@gridy = @grid.length
+        #@gridx = @grid[0].length
+    #end
+    #def [](y, x)
+        #if x < 0 || y < 0 || y >= @grid.length || x >= @grid[0].length || @grid[y][x].nil? then
+            #return nil
+        #else
+            #return @grid[y][x]
+        #end
+    #end
+    #def occupied?(y, x)
+        #return (not self[y,x].nil?)
+    #end
 #end
+
+#$dngn = Dungeon.new(Dungeon.parse("dungeon.txt"))
 
 $map = [[?#, ?#, ?#, ?#, ?#,  0,  0,  0,  0,  0],
         [?#,  0,  0,  0, ?#, ?#, ?#, ?#, ?#,  0],
@@ -16,6 +53,35 @@ $map = [[?#, ?#, ?#, ?#, ?#,  0,  0,  0,  0,  0],
 
 $mey = $map.index { |r| $mex = r.index { |x| x == ?@ } }
 
+def parse_dun(fn)
+    File.open(fn, ?r) do |f|
+        return f.each_line.map do |ln|
+            ln.each_char.map do |c|
+                case c
+                when ?#
+                    ?#
+                when ?@
+                    ?@
+                when ?k
+                    ?k
+                when ?|
+                    ?|
+                when ?*
+                    ?*
+                else
+                    nil
+                end
+            end
+        end
+    end
+end
+
+$map = parse_dun("dungeon.txt")
+
+
+$mey = $map.index { |r| $mex = r.index { |x| x == ?@ } }
+$havekey = false
+
 def keyboard_controls()
     ev = Termbox::Event.new
     if Termbox.tb_poll_event(ev) >= 0 && ev[:type] == 1 then
@@ -23,26 +89,26 @@ def keyboard_controls()
         when 0x1B
             exit
         when 0xFFFF-18
-            if not occupied? $mex, $mey-1 then
-                $map[$mey][$mex]=0
+            if not collision? $mex, $mey-1 then
+                $map[$mey][$mex]=nil
                 $mey-=1
                 $map[$mey][$mex]=?@
             end
         when 0xFFFF-19
-            if not occupied? $mex, $mey+1 then
-                $map[$mey][$mex]=0
+            if not collision? $mex, $mey+1 then
+                $map[$mey][$mex]=nil
                 $mey+=1
                 $map[$mey][$mex]=?@
             end
         when 0xFFFF-20
-            if not occupied? $mex-1, $mey then
-                $map[$mey][$mex]=0
+            if not collision? $mex-1, $mey then
+                $map[$mey][$mex]=nil
                 $mex-=1
                 $map[$mey][$mex]=?@
             end
         when 0xFFFF-21
-            if not occupied? $mex+1, $mey then
-                $map[$mey][$mex]=0
+            if not collision? $mex+1, $mey then
+                $map[$mey][$mex]=nil
                 $mex+=1
                 $map[$mey][$mex]=?@
             end
@@ -60,18 +126,30 @@ def keyboard_controls()
     end
 end
 
+def collision?(x, y)
+    if $map[y][x] == ?k then $havekey = true end
+    if $map[y][x] == ?* then win end
+    return ($map[y][x] == ?#) || ($map[y][x] == ?| && $havekey==false)
+end
+
+def win
+    exit
+end
+
 def occupied?(x, y)
-    return (not ($map[y][x] == 0))
+    return (not ($map[y][x] == nil))
 end
 
 def display()
     Termbox.tb_clear
     $map.each_index do |y|
         $map[y].each_index do |x|
-            Termbox.tb_change_cell x, y, $map[y][x].ord, 0, 0
+            if not $map[y][x].nil? then
+                Termbox.tb_change_cell x, y, $map[y][x].ord, 0, 0
+            end
         end
     end
-    Termbox.tb_change_cell $mex, $mey, ?@.ord, 0, 0
+    #Termbox.tb_change_cell $mex, $mey, ?@.ord, 0, 0
     Termbox.tb_present
 end
 
