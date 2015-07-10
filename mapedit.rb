81e0c1fcf088a9da6505a26cecb1235d869eac27
Owen Lynch <oclynch888@gmail.com>
2015-07-10 12:10:55 -0400

    indentation fixed

--- mapedit.rb
+++ mapedit.rb
@@ -1,72 +1,73 @@
-lass Thing
-	attr_accessor :map, :y, :x
-	def ch
-		nil
-	end
+class Thing
+  attr_accessor :map, :y, :x
+  def ch
+    nil
+  end
 end
 
 class Player < Thing
-	def initialize(attr)
-		@attr = attr
-	end
+  def initialize(attr)
+    @attr = attr
+    @inventory = []
+  end
 
-	def ch
-		?@
-	end
+  def ch
+    ?@
+  end
 end
 
 class Wall < Thing
-	def initialize
-	end
+  def initialize
+  end
 
-	def ch
-		?#
-	end
+  def ch
+    ?#
+  end
 end
 
 class Key < Thing
-	def initialize(door)
-		@door = door
-	end
+  def initialize(door)
+    @door = door
+  end
 
-	def open
-		@door.open
-	end
+  def open
+    @door.open
+  end
 
-	def ch
-		?k
-	end
+  def ch
+    ?k
+  end
 end
 
 class Door < Thing
-	attr_accessor :open
-	def initialize
-		@open = false
-	end
+  attr_accessor :open
+  def initialize
+    @open = false
+  end
 
-	def ch
-		if @open ?| else nil end
-	end
+  def ch
+    if @open ?| else nil end
+  end
 end
 
 class Map
-	def initialize(y, x, me)
-		@grid = Array.new(y * x)
-		@ydim = y
-		@xdim = x
-	end
+  def initialize(y, x, me)
+    @grid = Array.new(y * x)
+    @ydim = y
+    @xdim = x
+  end
 
-	def [](y, x)
-		@grid[y * @xdim + x] unless y >= @ydim or y < 0 or x >= @xdim or x < 0
-	end
+  def [](y, x)
+    @grid[y * @xdim + x] unless y >= @ydim or y < 0 or x >= @xdim or x < 0
+  end
 
-	def []=(y, x, thing)
-		if y >= @ydim or y < 0 or x >= @xdim or x < 0
-			return nil
-		end
-		thing.map = self
-		thing.y = y
-		thing.x = x
-		@grid[y * @xdim + x] = thing
-	end
+  def []=(y, x, thing)
+    if y >= @ydim or y < 0 or x >= @xdim or x < 0
+      return nil
+    end
+    thing.map = self
+    thing.y = y
+    thing.x = x
+    @grid[y * @xdim + x] = thing
+  end
 end
