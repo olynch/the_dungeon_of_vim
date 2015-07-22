82389f493ac4d1b1d375bfe241ccdf8345acc042
Radvendii <dontsendmejunk@mailinator.com>
2015-07-22 16:36:22 -0400

    removing outdated code, updating to use map.rb

--- main.rb
+++ main.rb
@@ -3,6 +3,30 @@ require './maptest.rb'
 
 Termbox.initialize_library
 
+class Thing
+  def collision!(p)
+  end
+end
+
+class Key
+  def collision!(p)
+    p << self
+  end
+end
+
+class Door
+  def collision!(p)
+    if p.inventory.find_index {|k| k.is_a?(Key) && k.door_name == self.name}
+      self.open = true
+    end
+  end
+end
+
+class NilClass
+  def collision!(p)
+  end
+end
+
 def parse_dun(fn)
   File.open(fn, ?r) do |f|
     return f.each_line.map do |ln|
@@ -33,60 +57,34 @@ def keyboard_controls()
     when 0x1B
       exit
     when 0xFFFF-18
+      Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y-1].collision! Maptest::JOHN
       unless Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y-1].collision? then
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = nil
         Maptest::JOHN.y-=1
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = Maptest::JOHN
       end
-      #if not collision? $mex, $mey-1 then
-      #$map[$mey][$mex]=nil
-      #$mey-=1
-      #$map[$mey][$mex]=?@
-      #end
     when 0xFFFF-19
+      Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y+1].collision! Maptest::JOHN
       unless Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y+1].collision? then
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = nil
         Maptest::JOHN.y+=1
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = Maptest::JOHN
       end
-      #if not collision? $mex, $mey+1 then
-      #$map[$mey][$mex]=nil
-      #$mey+=1
-      #$map[$mey][$mex]=?@
-      #end
     when 0xFFFF-20
+      Maptest::MAP[Maptest::JOHN.x-1, Maptest::JOHN.y].collision! Maptest::JOHN
       unless Maptest::MAP[Maptest::JOHN.x-1, Maptest::JOHN.y].collision? then
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = nil
         Maptest::JOHN.x-=1
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = Maptest::JOHN
       end
-      #if not collision? $mex-1, $mey then
-      #$map[$mey][$mex]=nil
-      #$mex-=1
-      #$map[$mey][$mex]=?@
-      #end
     when 0xFFFF-21
+      Maptest::MAP[Maptest::JOHN.x+1, Maptest::JOHN.y].collision! Maptest::JOHN
       unless Maptest::MAP[Maptest::JOHN.x+1, Maptest::JOHN.y].collision? then
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = nil
         Maptest::JOHN.x+=1
         Maptest::MAP[Maptest::JOHN.x, Maptest::JOHN.y] = Maptest::JOHN
       end
-      #if not collision? $mex+1, $mey then
-      #$map[$mey][$mex]=nil
-      #$mex+=1
-      #$map[$mey][$mex]=?@
-      #end
     end
-    #case ev[:ch]
-    #when ?k.ord
-    #$mey-=1
-    #when ?j.ord
-    #$mey+=1
-    #when ?h.ord
-    #$mex-=1
-    #when ?l.ord
-    #$mex+=1
-    #end
     move_enemy
   end
 end
@@ -111,59 +109,13 @@ def move_enemy()
   $map = buffer
 end
 
-def collision?(x, y)
-  if $map[y][x] == ?k then $items << "key" end
-  if $map[y][x] == ?| && $items.include?("key") then
-    $items.delete_at($items.find_index("key"))
-    $map[y][x] = nil
-  end
-  if $map[y][x] == ?* then win end
-  return $map[y][x] == ?# || $map[y][x] == ?|
-end
-
 def win
   exit
-end
-
-def visible?(p0, p1)
-  [[0,1], [1,0]].each do |c|
-    if p0[c[0]]<p1[c[0]] then
-      p0[c[0]].upto(p1[c[0]]-1)
-    else p0[c[0]].downto(p1[c[0]]+1)
-    end
-    .each do |c0|
-      c1 =  (((p1[c[1]]-p0[c[1]])*(c0-p0[c[0]])).fdiv(p1[c[0]]-p0[c[0]])+p0[c[1]]) #no divide-by-zero problem because x1.upto(x2-1) won't run anything if x1==x2
-      if [[c1.ceil, c0], [c1.floor, c0]].all? do |e|
-        ($map[e[c[0]]][e[c[1]]] == ?# || $map[e[c[0]]][e[c[1]]] == ?|)
-      end
-      then
-      return false
-      end
-    end
-  end
-  return true
-end
-
-def occupied?(x, y)
-  return (not ($map[y][x] == nil))
 end
 
 def display()
   Termbox.tb_clear
   Maptest::JOHN.display
-  #$map.each_index do |y|
-  #$map[y].each_index do |x|
-  #if not $map[y][x].nil? then
-  #if visible? [$mex, $mey], [x, y] then
-  #Termbox.tb_change_cell x, y, $map[y][x].ord, 4, 0
-  #$map[y][x].instance_variable_set(:@seen, true)
-  #elsif $map[y][x].instance_variable_get(:@seen) then
-  #Termbox.tb_change_cell x, y, $map[y][x].ord, 1, 0
-  #end
-  #end
-  #end
-  #end
-  #disptext($map.length, 0)
   Termbox.tb_present
 end
 
