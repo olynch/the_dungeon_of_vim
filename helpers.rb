1a244398584bc2d85a9a142f73250f66907e5d63
Radvendii <dontsendmejunk@mailinator.com>
2015-07-24 02:43:50 -0400

    more array methods. sigh.

--- helpers.rb
+++ helpers.rb
@@ -2,4 +2,11 @@ class Array
   def delete_first(obj)
     self.delete_at(self.index(obj))
   end
+
+  def find_object
+    self.each do |x|
+      if yield x then return x end
+    end
+    return nil
+  end
 end
