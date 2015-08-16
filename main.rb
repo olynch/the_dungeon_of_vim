d716388b3ccd09f52f7ae091e15f68424ce04de1
Radvendii <dontsendmejunk@mailinator.com>
2015-08-16 10:37:54 -0400

    removed legacy code

--- main.rb
+++ main.rb
@@ -5,21 +5,6 @@ require './maptest.rb'
 
 Termbox.initialize_library
 
-def parse_dun(fn)
-  File.open(fn, ?r) do |f|
-    return f.each_line.map do |ln|
-      ln.each_char.map do |c|
-        if [?#, ?@, ?k, ?|, ?*, ?e].include? c then
-          c
-        else
-          nil
-        end
-      end
-    end
-  end
-end
-
-
 def keyboard_controls()
   ev = Termbox::Event.new
   if Termbox.tb_poll_event(ev) >= 0 && ev[:type] == 1 then
@@ -64,11 +49,10 @@ end
 
 begin
   Termbox.tb_init
-  #Display::AREAS << Display::AreaRectangle.new([10,1], [15,11], Maptest::JOHN.method("disp"))
-  #Display::AREAS << Display::AreaRectangle.new([1,1], [21,11], Maptest::JOHN.method("disp"), :default)
+  Display::AREAS << Display::AreaRectangle.new([1,1], [21,11], Maptest::JOHN.method("disp"), :default)
   Display::AREAS << test= Display::AreaCircle.new(10,10, 5, Maptest::JOHN.method("disp"), :default)
-  #Display::AREAS << Display::AreaRectangle.new([23,1], [43,11], Maptest::MAP.method("disp"), :default)
-  #Display::AREAS << Display::AreaRectangle.new([50,1], [100,50], proc {Display.text.disp}, Display::Shift << Display::Clip << Display::Tail << Display::Wrap)
+  Display::AREAS << Display::AreaRectangle.new([23,1], [43,11], Maptest::MAP.method("disp"), :default)
+  Display::AREAS << Display::AreaRectangle.new([50,1], [100,50], proc {Display.text.disp}, Display::Shift << Display::Clip << Display::Tail << Display::Wrap)
   #Display::AREAS << Display::Area.new(20, 20, proc{Display.text.disp})
   #Display::AREAS << test = Display::AreaWrap.new(2, 2, proc {Display.text.disp}, border: (-1..10).flat_map {|i| [[i,-1], [-1,i], [10,i], [i,10]]}.uniq)
   p test.border
